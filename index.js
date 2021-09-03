24411686ebd4736ebadcbbd15662d5617e7ed4bb
Md fakih <mf9049@gmail.com>
2021-09-03 17:12:24 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 let count = 0
 let countEl = document.getElementById("count-el")
 let paraEl = document.getElementById("para-el")
-
+let sumEl = document.getElementById("sum-el")
+let total = []
 function increment(){
     count += 1
     countEl.textContent = count
  }
 
+function zero(){
+   countEl.textContent = 0
+   count = 0
+}
+
  function save(){
     paraEl.textContent += count + "-"
-    countEl.textContent = 0
-    count = 0
+    total.push(count)
+    sumEl.textContent = "Total:" + total.reduce((a, b) => a + b, 0)
+    zero()
  }
  function reset(){
     paraEl.textContent = "Previous enteries:"
-    countEl.textContent = 0
-    count = 0
+    zero()
+    total = []
+    sumEl.textContent = "Total:"
  }
